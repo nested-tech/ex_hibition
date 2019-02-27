0a4c06dd8ac67d64728b8f3918998eb382ab85f9
Francesco <lofrancofrancesco@gmail.com>
2019-02-27 23:44:29 +0000

    Add FAC test case

--- test/ex_hibition_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ExHibitionTest do
-  use ExUnit.Case
-  doctest ExHibition
-
-  test "greets the world" do
-    assert ExHibition.hello() == :world
-  end
-end
---
+++ test/fac_test.exs
@@ -0,0 +1,4 @@
+defmodule FacTest do
+  use ExUnit.Case, async: true
+  doctest Fac
+end
